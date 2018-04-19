cef986e0fff00cb9bc157a1cfd02a057bfc81e92
rogiertenhacken <rogiertenhacken@gmail.com>
2018-04-19 21:51:57 +0200

    added-basic-carousel

---
+++ myscript.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $(function() {
+  $(".owl-carousel.std").owlCarousel({
+    loop: true,
+    nav: true,
+    navText: '<>',
+    margin: 0,
+    autoWidth: true,
+    center: true,
+    dots: true
+  });
+});
+});
