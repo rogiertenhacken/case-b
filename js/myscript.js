15fb64f48968b4489d3a810f9a6e904d3695bf87
rogiertenhacken <rogiertenhacken@gmail.com>
2018-04-22 10:23:03 +0200

    indent-js-file

--- js/myscript.js
+++ js/myscript.js
@@ -1,20 +1,20 @@
 $(document).ready(function() {
   $(function() {
-  $(".owl-carousel.std").owlCarousel({
-    loop:true,
-    autoplay:true,
-    autoplayTimeout:3500,
-    autoplayHoverPause:true,
-    smartSpeed:1500,
-    navigation:true,
-    nav: true,
-    navText:'<>',
-    navSpeed:1000,
-    items:1,
-    margin:0,
-    autoWidth:true,
-    center:true,
-    dots:false
+    $(".owl-carousel.std").owlCarousel({
+      loop:true,
+      autoplay:true,
+      autoplayTimeout:3500,
+      autoplayHoverPause:true,
+      smartSpeed:1500,
+      navigation:true,
+      nav: true,
+      navText:'<>',
+      navSpeed:1000,
+      items:1,
+      margin:0,
+      autoWidth:true,
+      center:true,
+      dots:false
+    });
   });
-});
 });
